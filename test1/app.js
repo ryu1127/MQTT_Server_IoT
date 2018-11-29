03872aa5dc819854548bba61ee2a11d97cd3cc8a
ryu <ryudonghun78@gmail.com>
2018-11-30 01:49:30 +0900

    nodejs server update for arduino mqtt update topic

--- test1/app.js
+++ test1/app.js
@@ -40,23 +40,93 @@ app.use(bodyParser.json());
 ******************************************/
 var mqtt = require('mqtt');
 //Broker's IP Address
-var mqttClient = mqtt.connect('mqtt://192.168.0.9:1883');
+var mqttClient = mqtt.connect('mqtt://localhost:1883');
 
 /**log**/
 console.log('mqtt Successfully Connect...');
 
 /***************************************** 
+** Configure for MQTT Publish Event!
+******************************************/
+//Publish Event
+mqttClient.on('connect', () => {
+  console.log('Connected to MQTT for Publishing...');
+});
+
+console.log('MQTT Handler Settings Complete...');
+
+/***************************************** 
+** Subscribe MQTT Topics
+******************************************/
+mqttClient.subscribe('sensor_values/fine_dust/uid/current_value');
+mqttClient.subscribe('sensor_values/humidity/uid/current_value');
+mqttClient.subscribe('sensor_values/illuminance/uid/current_value');
+mqttClient.subscribe('sensor_values/temperature/uid/current_value');
+mqttClient.subscribe('iot_state/uid/air_cleaner/state');
+mqttClient.subscribe('iot_state/uid/air_conditioner/state');
+mqttClient.subscribe('iot_state/uid/dehumidifier/state');
+mqttClient.subscribe('iot_state/uid/doorlock/state');
+mqttClient.subscribe('iot_state/uid/table/state');
+mqttClient.subscribe('iot_state/uid/window/state');
+
+
+// mqttClient.subscribe('users/custom_uid/name');
+
+
+/***************************************** 
 ** MQTT Handler Settings !
 ******************************************/
-
-mqttClient.subscribe('lockers_info/custom_uid/password');
-mqttClient.subscribe('users/custom_uid/name');
 // Subscribe event
 mqttClient.on('message', (topic,message) => {
   console.log('Subscribe MQTT *******');
   topic = topic.split('/');
   console.log('topic : '+topic);
   console.log(message.toString());
+
+    //update firebase database
+    // console.log('update firebase database...');
+    // var usersRef = firebase.database().ref().child(topic[0]+'/'+topic[1]);
+    // usersRef.update({
+    //   name : message.toString()
+    // });
+
+    console.log('update complete...');
+    console.log('firebase Completely update !!');
+
+  if(topic[0]=='sensor_values'){
+    // if(topic[1]=='fine_dust'){
+    //   var userRef = firebase.database().ref().child(topic[0]+'/'+topic[1]+'/'+topic[2]);
+    //   userRef.update({
+    //     current_value : message.toString()
+    //   })
+    // }else if(topic[1]=='humidity'){
+    //   var userRef = firebase.database().ref().child(topic[0]+'/'+topic[1]+'/'+topic[2]);
+    //   userRef.update({
+    //     current_value : message.toString()
+    //   })
+    // }else if(topic[1]=='illuminance'){
+    //   var userRef = firebase.database().ref().child(topic[0]+'/'+topic[1]+'/'+topic[2]);
+    //   userRef.update({
+    //     current_value : message.toString()
+    //   })
+    // }else if(topic[1]=='temperature'){
+    //   var userRef = firebase.database().ref().child(topic[0]+'/'+topic[1]+'/'+topic[2]);
+    //   userRef.update({
+    //     current_value : message.toString()
+    //   })
+    // }
+    var userRef = firebase.database().ref().child(topic[0]+'/'+topic[1]+'/'+topic[2]);
+    userRef.update({
+      current_value : message.toString()
+    })
+  }
+  if(topic[0]=='iot_state'){
+    var userRef = firebase.database().ref().child(topic[0]+'/'+topic[1]+'/'+topic[2]);
+    userRef.update({
+      state : message.toString()
+    })
+  }
+
 
   // //set firebase database
   // console.log('set firebase database...');
@@ -67,21 +137,17 @@ mqttClient.on('message', (topic,message) => {
   // console.log('set complete...');
 
   //update firebase database
-  console.log('update firebase database...');
-  var usersRef = firebase.database().ref().child(topic[0]+'/'+topic[1]);
-  usersRef.update({
-    name : message.toString()
-  });
+  // console.log('update firebase database...');
+  // var usersRef = firebase.database().ref().child(topic[0]+'/'+topic[1]);
+  // usersRef.update({
+  //   name : message.toString()
+  // });
   console.log('update complete...');
   console.log('firebase Completely update !!');
 });
 
-//Publish Event
-mqttClient.on('connect', () => {
-  console.log('Connected to MQTT for Publishing...');
-});
 
-console.log('MQTT Handler Settings Complete...');
+
 
 
 /***************************************** 
@@ -105,17 +171,128 @@ console.log('Firebase Initialize App Complete...');
   /**log**/
 console.log('Firebase Database Connection Settings Complete...');
 
-/***************************************** 
-** Firebase listener Settings  !
-******************************************/
+/********************************************************************************** 
+************************* Firebase listener Settings  !
+***********************************************************************************/
 console.log('Firebase listener settings start...');
 
+/***************************************** 
+** Door Lock Password Listener !
+******************************************/
 // State Change Listener
-var doorStateRef = firebase.database().ref('lockers_info/custom_uid/password');
+var passwordRef = firebase.database().ref('iot_state/uid/doorlock/password');
+passwordRef.on('value',function(snapshot){
+  console.log('password update! : ' + snapshot.val());
+  mqttClient.publish('iot_state/uid/doorlock/password',snapshot.val().toString());
+});
+/***************************************** 
+** Door Lock State Listener !
+******************************************/
+var doorStateRef = firebase.database().ref('iot_state/uid/doorlock/state');
 doorStateRef.on('value',function(snapshot){
-  console.log('password update!');
-  console.log(snapshot.val());
-})
+  console.log('doorlock : '+ snapshot.val());
+  mqttClient.publish('iot_state/uid/doorlock/state',snapshot.val().toString());
+});
+/***************************************** 
+** air conditioner State Listener !
+******************************************/
+var airconStateRef = firebase.database().ref('iot_state/uid/air_conditioner/state');
+airconStateRef.on('value',function(snapshot){
+  console.log('aircon State : '+ snapshot.val());
+});
+/***************************************** 
+** air cleaner State listener !
+******************************************/
+var cleanerStateRef = firebase.database().ref('iot_state/uid/air_cleaner/state');
+cleanerStateRef.on('value',function(snapshot){
+  console.log('cleaner State : '+ snapshot.val());
+});
+/***************************************** 
+** dehumidifier State listener !
+******************************************/
+var dehumStateRef = firebase.database().ref('iot_state/uid/dehumidifier/state');
+dehumStateRef.on('value',function(snapshot){
+  console.log('dehumidifier State : '+ snapshot.val());
+});
+/***************************************** 
+** table State listener !
+******************************************/
+var tableStateRef = firebase.database().ref('iot_state/uid/table/state');
+tableStateRef.on('value',function(snapshot){
+  console.log('table : '+ snapshot.val());
+});
+/***************************************** 
+** window State listener !
+******************************************/
+var windowStateRef = firebase.database().ref('iot_state/uid/window/state');
+windowStateRef.on('value',function(snapshot){
+  console.log('window : '+ snapshot.val());
+});
+/***************************************** 
+** SENSOR(fine_dust) listener !
+******************************************/
+var sensorRef = firebase.database().ref('sensor_values/fine_dust/uid/current_value');
+windowStateRef.on('value',function(snapshot){
+  console.log('fine_dust : '+ snapshot.val());
+  var val = snapshot.val();
+  //숫자로 변형
+  val *= 1;
+  //미세먼지 최악
+  if(val <= 200){
+    if(val <= 150){
+      if(val <= 100){
+        if(val <= 50){
+          //최고로 좋음
+          //창문 닫혀있다면 열기
+        }
+        //창문 열려있다면 닫기
+        //나쁨
+      }
+      //창문 열려있다면 닫기
+      //상당히 나쁨
+    }
+    //창문 열려있다면 닫기
+    //최악일 때 할 것
+  }
+});
+/***************************************** 
+** SENSOR(humidity) listener !
+******************************************/
+var sensorRef = firebase.database().ref('sensor_values/humidity/uid/current_value');
+windowStateRef.on('value',function(snapshot){
+  console.log('humidity : '+ snapshot.val());
+  //습도는 0~100%까지 존재
+  var val = snapshot.val();
+  val *= 1;
+  if(val >= 50){
+    //제습기가 가동중이 아니라면
+    //제습기 가동
+  }
+  else if(val < 30){
+    //제습기가 가동중이라면 
+    //제습기 정지
+  }
+
+});
+/***************************************** 
+** SENSOR(illuminance) listener !
+******************************************/
+var sensorRef = firebase.database().ref('sensor_values/illuminance/uid/current_value');
+windowStateRef.on('value',function(snapshot){
+  console.log('illuminance : '+ snapshot.val());
+  var val = snapshot.val();
+  val *= 1;
+});
+/***************************************** 
+** SENSOR(temperature) listener !
+******************************************/
+var sensorRef = firebase.database().ref('sensor_values/temperature/uid/current_value');
+windowStateRef.on('value',function(snapshot){
+  console.log('temperature : '+ snapshot.val());
+  var val = snapshot.val();
+  val *= 1;
+});
+
 
 
 
@@ -132,7 +309,6 @@ console.log('Port Settings Complete...');
 /***************************************** 
 ** Route
 ******************************************/
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
